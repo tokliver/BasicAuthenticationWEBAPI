27d203abf788e54f198204aea11d0f6796468d1c
tokliver <tokliver@gmail.com>
2020-03-28 19:49:22 +0100

    Ajoutez des fichiers projet.

---
+++ BasicAuthenticationWEBAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using BasicAuthenticationWEBAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BasicAuthenticationWEBAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+            config.Filters.Add(new BasicAuthenticationAttribute());
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
